92261385403f933df2f68e7025fe671b7f3504b8
Galttu <58257767+Galttu@users.noreply.github.com>
2021-04-04 12:38:04 +0300

    Projektin neljäs vaihe

--- angulardemo/src/app/admin/accounts/accounts.module.ts
+++ angulardemo/src/app/admin/accounts/accounts.module.ts
@@ -1,11 +1,8 @@
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import {MatTableModule} from '@angular/material/table'; 
-import { MatPaginator } from '@angular/material/paginator';
-import {DataSource} from '@angular/cdk/collections';
-
-
+import { MatTableModule } from '@angular/material/table'; 
+import { MatSortModule } from '@angular/material/sort';
 import { MatPaginatorModule } from '@angular/material/paginator'; 
 import { AccountsRoutingModule } from './accounts-routing.module';
 import { ListComponent } from './list.component';
@@ -18,8 +15,7 @@ import { AddEditComponent } from './add-edit.component';
         AccountsRoutingModule,
         MatTableModule,
         MatPaginatorModule,
-        
-
+        MatSortModule        
     ],
     declarations: [
         ListComponent,
--- angulardemo/src/app/admin/accounts/list.component.ts
+++ angulardemo/src/app/admin/accounts/list.component.ts
@@ -1,25 +1,28 @@
-﻿import {AfterViewInit, Component, NgModule, OnInit, ViewChild } from '@angular/core';
+﻿import { Component, NgModule, OnInit, ViewChild } from '@angular/core';
 import { first } from 'rxjs/operators';
-import {MatTableDataSource} from '@angular/material/table';
+import { MatTableDataSource } from '@angular/material/table';
 import { AccountService } from '@app/_services';
-import { Account } from '@app/_models';
-import { MatPaginator } from '@angular/material/paginator';
+import { MatPaginator } from '@angular/material/paginator'; // paginointia varten importtaus
+import { MatSort } from '@angular/material/sort'; // sort toimintoa varten importtaus
 @Component({ templateUrl: 'list.component.html' })
 
 export class ListComponent implements OnInit  {
-    displayedColumns: string[] = ['name','email','role','edit'];
+    displayedColumns: string[] = ['name','email','role','edit']; // list.component.html sivuston näytettävät kentät
 
     accounts: any[];
-    dataSource = new MatTableDataSource<any>();
-    @ViewChild(MatPaginator) paginator: MatPaginator;
+    dataSource = new MatTableDataSource<any>(); // taulukkoa, paginointtia ja sorttia varten dataSource sisällön määrittely
+    @ViewChild(MatPaginator) paginator: MatPaginator;   // paginointi näkyvyys
+    @ViewChild(MatSort) sort: MatSort;  // sort näkyvyys
+
     constructor(private accountService: AccountService) {}
 
     ngOnInit() {
         this.accountService.getAll() 
             .pipe(first())
             .subscribe(accounts => {
-                this.dataSource.data = accounts; 
+                this.dataSource.data = accounts; // tilataan observableilta dataa
                 this.dataSource.paginator = this.paginator;
+                this.dataSource.sort = this.sort;
             });
     }
     deleteAccount(id: string) {
@@ -30,6 +33,5 @@ export class ListComponent implements OnInit  {
             .subscribe(() => {
                 this.accounts = this.accounts.filter(x => x.id !== id) 
             });
-    }
-    
+    }   
 }
